e8b140eeaff6f6182bfe25920a7874d593cf16b6
longtaoge <61852263@qq.com>
2015-03-04 17:42:53 +0800

    Popups  添加注释

--- www/$ionicPopup/popping/index.js
+++ www/$ionicPopup/popping/index.js
@@ -1,85 +1,122 @@
-
 angular.module('popping', ['ionic'])
-.controller('PopupCtrl', function($scope, $timeout, $q, $ionicPopup) {
-  $scope.showPopup = function() {
-    $scope.data = {}
+    .controller('PopupCtrl', function ($scope, $timeout, $q, $ionicPopup) {
+        $scope.showPopup = function () {
+            $scope.data = {}
 
-    $ionicPopup.show({
-      templateUrl: 'popup-template.html',
-      title: 'Enter Wi-Fi Password',
-      subTitle: 'Please use normal things',
-      scope: $scope,
-      buttons: [
-        { text: 'Cancel', onTap: function(e) { return true; } },
-        {
-          text: '<b>Save</b>',
-          type: 'button-positive',
-          onTap: function(e) {
-            return $scope.data.wifi;
-          }
-        },
-      ]
-      }).then(function(res) {
-        console.log('Tapped!', res);
-      }, function(err) {
-        console.log('Err:', err);
-      }, function(msg) {
-        console.log('message:', msg);
-      });
+            $ionicPopup.show({
+                templateUrl: 'popup-template.html',
+                title: '输入 上网  密码',
+                subTitle: '请注意周边环境，防止密码被盗',
+                scope: $scope,
+                buttons: [
+                    {
+                        text: '取消', onTap: function (e) {
+                        return true;
+                    }
+                    },
+                    {
+                        text: '<b>保存</b>',
+                        type: 'button-positive',
+                        onTap: function (e) {
+                            return $scope.data.wifi;
+                        }
+                    }
+                ]
+            }).then(function (res) {
+                console.log('Tapped!', res);
+            }, function (err) {
+                console.log('Err:', err);
+            }, function (msg) {
+                console.log('message:', msg);
+            });
 
-    $timeout(function() {
-      $ionicPopup.confirm({
-        title: 'Do you like ice cream?',
-        cancelText: 'No',
-        okText: 'Yes, of course'
-      }).then(function(res) {
-        console.log('Your love for ice cream:', res);
-      });
-    }, 1000);
-  };
+            $timeout(function () {
+                $ionicPopup.confirm({
+                    title: '超时了，你在吃冰糕吗?',
+                    cancelText: '没有',
+                    okText: '是的'
+                }).then(function (res) {
+                    console.log('Your love for ice cream:', res);
+                });
+            }, 5000);
+        };
 
-  $scope.showConfirm = function() {
-    $ionicPopup.confirm({
-      title: 'Consume Ice Cream',
-      content: 'Are you sure you want to eat this ice cream?'
-    }).then(function(res) {
-      if(res) {
-        console.log('You are sure');
-      } else {
-        console.log('You are not sure');
-      }
+        $scope.showConfirm = function () {
+            $ionicPopup.confirm({
+                title: '定制冰糕',
+                content: '你真的要这种口味的冰糕吗?',
+
+                buttons: [
+                    {
+                        text: '取消', onTap: function (e) {
+                        return true;
+                    }
+                    },
+                    {
+                        text: '<b>确定</b>',
+
+                        onTap: function (e) {
+                            return true;
+                        }
+                    }
+                ]
+            }).then(function (res) {
+                if (res) {
+                    console.log('You are sure');
+                } else {
+                    console.log('You are not sure');
+                }
+            });
+        };
+        $scope.showPrompt = function () {
+            $ionicPopup.prompt({
+                title: '标题',
+                subTitle: '副标题',
+
+                buttons: [
+                    {
+                        text: '取消', onTap: function (e) {
+                        return true;
+                    }
+                    },
+                    {
+                        text: '<b>确定</b>',
+
+                        onTap: function (e) {
+                            return true;
+                        }
+                    }
+                ]
+            }).then(function (res) {
+                console.log('', res);
+            });
+        };
+        $scope.showPasswordPrompt = function () {
+            $ionicPopup.prompt({
+                title: '密码确认',
+                subTitle: '请输入您的解锁密码',
+                inputType: 'password',
+                inputPlaceholder: '请输入您的密码',
+
+
+            }).then(function (res) {
+                console.log(' ', res);
+            });
+        };
+        $scope.showAlert = function () {
+            $ionicPopup.alert({
+                title: '保存',
+                content: '您的数据已经保存!'
+
+            }).then(function (res) {
+                    console.log(' 您的数据已经保存!');
+                }, function (err) {
+                },
+                function (popup) {
+                    console.log('Got popup', popup);
+                    $timeout(function () {
+                        popup.close();
+                    }, 1000);
+                });
+        };
     });
-  };
-  $scope.showPrompt = function() {
-    $ionicPopup.prompt({
-      title: 'ID Check',
-      subTitle: 'What is your name?'
-    }).then(function(res) {
-      console.log('Your name is', res);
-    });
-  };
-  $scope.showPasswordPrompt = function() {
-    $ionicPopup.prompt({
-      title: 'Password Check',
-      subTitle: 'Enter your secret password',
-      inputType: 'password',
-      inputPlaceholder: 'Your password'
-    }).then(function(res) {
-      console.log('Your name is', res);
-    });
-  };
-  $scope.showAlert = function() {
-    $ionicPopup.alert({
-      title: 'Draft Saved',
-      content: 'Your Data has been saved!'
-    }).then(function(res) {
-      console.log('Your Data has been saved!');
-    }, function(err) {},
-    function(popup) {
-      console.log('Got popup', popup);
-      $timeout(function() {
-        popup.close();
-      }, 1000);
-    });
-  };
-});
